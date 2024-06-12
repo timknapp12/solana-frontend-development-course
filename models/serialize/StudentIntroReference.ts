9924ac8e55d0c10a1356ded0a7b7e419b555c312
Tim Knapp <timknapp12@gmail.com>
2024-06-11 19:00:45 -0600

    first commit

--- models/serialize/StudentIntroReference.ts
+++ models/serialize/StudentIntroReference.ts
@@ -1,4 +1,4 @@
-import * as borsh from '@project-serum/borsh'
+import * as borsh from '@project-serum/borsh';
 
 /*
     Class Structure: It includes two properties, name and message, 
@@ -16,45 +16,45 @@ import * as borsh from '@project-serum/borsh'
 */
 
 export class StudentIntroReference {
-    name: string;
-    message: string;
+  name: string;
+  message: string;
 
-    constructor(name: string, message: string) {
-        this.name = name;
-        this.message = message;
-    };
+  constructor(name: string, message: string) {
+    this.name = name;
+    this.message = message;
+  }
 
-    borshInstructionSchema = borsh.struct([
-        borsh.u8('variant'),
-        borsh.str('name'),
-        borsh.str('message')
-    ]);
+  borshInstructionSchema = borsh.struct([
+    borsh.u8('variant'),
+    borsh.str('name'),
+    borsh.str('message'),
+  ]);
 
-    static borshAccountSchema = borsh.struct([
-        borsh.u8('initialized'),
-        borsh.str('name'),
-        borsh.str('message'),
-    ]);
+  static borshAccountSchema = borsh.struct([
+    borsh.u8('initialized'),
+    borsh.str('name'),
+    borsh.str('message'),
+  ]);
 
-    // serialize info when we send it to the blockchain
-    serialize(): Buffer {
-        const buffer = Buffer.alloc(1000);
-        this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer);
-        return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer));
-    };
+  // serialize info when we send it to the blockchain
+  serialize(): Buffer {
+    const buffer = Buffer.alloc(1000);
+    this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer);
+    return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer));
+  }
 
-    // deserialize info when we retrieve it from the blockchain
-    static deserialize(buffer?: Buffer): StudentIntroReference | null {
-        if (!buffer) {
-            return null;
-        }
+  // deserialize info when we retrieve it from the blockchain
+  static deserialize(buffer?: Buffer): StudentIntroReference | null {
+    if (!buffer) {
+      return null;
+    }
 
-        try {
-            const { name, message } = this.borshAccountSchema.decode(buffer);
-            return new StudentIntroReference(name, message);
-        } catch (error) {
-            console.log('Deserialization error:', error);
-            return null;
-        }
-    };
-};
+    try {
+      const { name, message } = this.borshAccountSchema.decode(buffer);
+      return new StudentIntroReference(name, message);
+    } catch (error) {
+      console.log('Deserialization error:', error);
+      return null;
+    }
+  }
+}
--- pages/wallets/starter.tsx
+++ pages/wallets/starter.tsx
@@ -1,7 +1,84 @@
-import BoilerPlate from '../../components/BoilerPlate';
+import * as React from 'react';
+import * as web3 from '@solana/web3.js';
+import * as walletAdapterReact from '@solana/wallet-adapter-react';
+import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
+import {
+   WalletModalProvider,
+   WalletMultiButton,
+} from '@solana/wallet-adapter-react-ui';
+require('@solana/wallet-adapter-react-ui/styles.css');
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 
-const Starter = () => (
-    <BoilerPlate />
-)
+const Starter = () => {
+   const [balance, setBalance] = React.useState<number | null>(0);
+
+   const endpoint = web3.clusterApiUrl('devnet');
+
+   const wallets = [
+      new walletAdapterWallets.PhantomWalletAdapter(),
+      new walletAdapterWallets.SolflareWalletAdapter(),
+      new walletAdapterWallets.BraveWalletAdapter(),
+   ];
+
+   const { connection } = walletAdapterReact.useConnection();
+   const { publicKey } = walletAdapterReact.useWallet();
+
+   React.useEffect(() => {
+      const getInfo = async () => {
+         if (connection && publicKey) {
+            const walletInfo = await connection.getAccountInfo(publicKey);
+            setBalance(walletInfo!.lamports / web3.LAMPORTS_PER_SOL);
+         }
+      };
+      getInfo();
+   }, [publicKey, connection]);
+
+   return (
+      <>
+         {/* provides a connection to the solana json rpc api */}
+         <walletAdapterReact.ConnectionProvider endpoint={endpoint}>
+            {/* makes the wallet adapter available to the entirety of our application (wrapped in this component) */}
+            <walletAdapterReact.WalletProvider wallets={wallets}>
+               {/* provides components to the wrapped application */}
+               <WalletModalProvider>
+                  <main className='min-h-screen text-white'>
+                     <div className='grid grid-cols-1 lg:grid-cols-4 gap-4 p-4'>
+                        <div className='col-span-1 lg:col-start-2 lg:col-end-4 rounded-lg bg-[#2a302f] h-60 p-4'>
+                           <div className='flex justify-between items-center'>
+                              <h2 className='text-3xl font-semibold'>
+                                 account info ✨
+                              </h2>
+                              {/* button component for connecting to solana wallet */}
+                              <WalletMultiButton className='!bg-helius-orange !rounded-xl hover:!bg-[#161b19] transition-all duration-200' />
+                           </div>
+
+                           <div className='mt-8 bg-[#222524] border-2 border-gray-500 rounded-lg p-2'>
+                              <ul className='p-2'>
+                                 <li className='flex justify-between'>
+                                    <p className='tracking-wider'>
+                                       Wallet is connected...
+                                    </p>
+                                    <p className='text-helius-orange italic font-semibold'>
+                                       {publicKey ? 'yes' : 'no'}
+                                    </p>
+                                 </li>
+
+                                 <li className='text-sm mt-4 flex justify-between'>
+                                    <p className='tracking-wider'>Balance...</p>
+                                    <p className='text-helius-orange italic font-semibold'>
+                                       {balance}
+                                    </p>
+                                 </li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </main>
+               </WalletModalProvider>
+            </walletAdapterReact.WalletProvider>
+         </walletAdapterReact.ConnectionProvider>
+      </>
+   );
+};
 
 export default Starter;
